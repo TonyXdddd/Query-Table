ddf1e7a04efca7c09843310ee4dbb483fb932689
Anton But <TonyXddddkai@gmail.com>
2024-03-21 17:59:33 +0200

    replace 'homepage' prop from package.json config, add 'base' prop to the BrowserRouter component

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/Query-Table/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
